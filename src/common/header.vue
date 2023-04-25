beddc666fb468c942be8942ebb1653602c0b7179
Romanticoseu <1251448395@qq.com>
2023-04-25 22:26:28 +0800

    完善顶部导航栏

--- src/common/header.vue
+++ src/common/header.vue
@@ -1,39 +1,35 @@
 <template>
     <div class="container">
-        <div class="header">
-            <h1>希望玉雕坊</h1>
-            
-        </div>
-        <div class="theme">
-            <a href="/">
-                <p><span style="color:black;margin-left:40px;">希望玉雕</span>.一心文玩</p>
-            </a>
-            
-        </div>
         <div class="header-nav">
-            <div class="content-nav">
-                <ul class="nav-list" clearfix>
+            <div class="tou">
+                <div class="logo">
+                    <a href="#/home">
+                        <img src="../assets/img/logo.png" alt="希望玉雕坊" width="350">
+                    </a>
+                </div>
+                <div class="content-nav">
+                    <ul class="nav-list" clearfix>
+                        <li class="nav-item">
+                            <a href="#/home">网站首页</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#/center">关于我们</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#/story">产品展示</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#/news">荣誉证书</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#/about">联系我们</a>
+                        </li>
                     <li class="nav-item">
-                        <a href="/">首页</a>
+                            <a href="#/verify">证书查验</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="#/center">产品中心</a>
-                       </li>
-                    <li class="nav-item">
-                        <a href="#/story">品牌故事</a>
-                       </li>
-                    <li class="nav-item">
-                        <a href="#/news">新闻动态</a>
-                       </li>
-                    <li class="nav-item">
-                        <a href="#/about">联系我们</a>
-                    </li>
-                   <li class="nav-item">
-                        <a href="#/verify">证书查验</a>
-                   </li>
-                </ul>
+                    </ul>
+                </div>
             </div>
-           
         </div>
      </div>
 </template>
@@ -43,7 +39,7 @@
     }
 </script>
 <style scode>
-    *{
+*{
     margin:0;
     padding:0;
     box-sizing: border-box;
@@ -51,76 +47,44 @@
 .container{
     width: 100%;
 }
-.header{
-    width: 100%;
-    height: 50px;
-    background: #F2F2F2;
+.logo {
+    margin-left: 500px;
+    position: absolute;
+    z-index: 9999;
 }
-.header h1{
-    text-align: center;
-    color: gray;
-    font-family: "宋体";
-   
+.tou{
+    margin: 0px auto;
 }
- .theme{
-     width: 100%;
-     height: 80px;
-    }
-    .theme a{
-        text-decoration: none;
-        color: gray;
-    }
-  .theme p{
-        text-align: center;
-        padding-top: 20px;
-        font-size: 28px;
-        font-family: "宋体"
- } 
-    .header-nav {
-     width: 100%;
-     height: 80px;
-     line-height: 100px;
-    }
-    .content-nav{
-        width: 800px;
-        margin: 0 auto;
-       
-    } 
-    .header .right-nav{
-        width: 250px;
-        height: 85px;
-        float: right;
-        vertical-align: middle;
-        margin-top:25px;
-    }
-    .right-nav .hd-login,.hd-reg{
-       background-color:orange;
-        color:#666 !imporment;
-        padding:6px 22px;
-        margin: 0 6px;
-        border-radius: 100px;
-    }
-    .right-nav a{
-        text-decoration:none;
-    }
-     .header-nav ul {
-        list-style: none;
-    }
-    .header-nav ul li{
-        float:  left;
-        margin-right: 60px; 
-    }
-    .header-nav ul a{
-        text-decoration: none;
-        color: gray;
-    } 
-    .clearfix{
-        content:" ";
-        display: block;
-        clear:both;
-        height:0;
-        visibility: hidden;
-    }
+.header-nav {
+    height: 120px;
+    line-height: 100px;
+    background: url(../assets/img/index_01.jpg) repeat-x;
+}
+.content-nav{
+    margin: 0 auto;
+    clear: both;
+    padding-left: 900px;
+    overflow: hidden;
+    
+} 
+.header-nav ul {
+    list-style: none;
+}
+.header-nav ul li{
+    float:  left;
+    margin-right: 60px; 
+}
+.header-nav ul a{
+    text-decoration: none;
+    color: white;
+} 
+.clearfix{
+    content:" ";
+    display: block;
+    clear:both;
+    height:0;
+    visibility: hidden;
+}
  .header-nav ul  a:hover{
             color: orchid;
     }
