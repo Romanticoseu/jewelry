eff8f80a0a2a6afe690bda7342e81b098c5c2b78
Romanticoseu <1251448395@qq.com>
2023-06-16 17:49:44 +0800

    update header

--- src/common/header.vue
+++ src/common/header.vue
@@ -1,37 +1,33 @@
 <template>
-    <div class="container">
-        <div class="header-nav">
-            <div class="tou">
-                <div class="logo">
-                    <a href="#/home">
-                        <img src="../assets/img/logo.png" alt="希望玉雕坊" width="350">
-                    </a>
-                </div>
-                <div class="content-nav">
-                    <ul class="nav-list" clearfix>
-                        <li class="nav-item">
-                            <a href="#/home">网站首页</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#/center">关于我们</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#/story">产品展示</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#/news">荣誉证书</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#/about">联系我们</a>
-                        </li>
-                    <li class="nav-item">
-                            <a href="#/verify">证书查验</a>
-                    </li>
-                    </ul>
-                </div>
+    <div class="header-nav">
+        <div class="content-nav">
+            <div class="logo">
+                <a href="#/home">
+                    <img src="../assets/img/logo.png" alt="希望玉雕坊" width="350">
+                </a>
             </div>
+            <ul class="nav-list" clearfix>
+                <li class="nav-item">
+                    <a href="#/home">网站首页</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#/center">关于我们</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#/story">产品展示</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#/news">荣誉证书</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#/about">联系我们</a>
+                </li>
+            <li class="nav-item">
+                    <a href="#/verify">证书查验</a>
+            </li>
+            </ul>
         </div>
-     </div>
+    </div>
 </template>
 <script>
     export default {
@@ -44,35 +40,31 @@
     padding:0;
     box-sizing: border-box;
 }
-.container{
-    width: 100%;
-}
-.logo {
-    margin-left: 500px;
-    position: absolute;
-    z-index: 9999;
-}
-.tou{
-    margin: 0px auto;
-}
 .header-nav {
-    height: 120px;
-    line-height: 100px;
+    width: 100%;
+    height: 130px;
     background: url(../assets/img/index_01.jpg) repeat-x;
 }
 .content-nav{
+    width: 1200px;
+    height: 130px;
     margin: 0 auto;
-    clear: both;
-    padding-left: 900px;
-    overflow: hidden;
-    
 } 
+.logo {
+    float: left;
+}
+.nav-list {
+    float: right;
+    margin: 50px;
+}
 .header-nav ul {
-    list-style: none;
+    height: 100px;
 }
 .header-nav ul li{
     float:  left;
-    margin-right: 60px; 
+    height: 130px;
+    width: 108px;
+    list-style: none;
 }
 .header-nav ul a{
     text-decoration: none;
